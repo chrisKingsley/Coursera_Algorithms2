f24080b8f65b5d9c5e511c16752c2feddb07c3e8
Chris Kingsley <kingsley.chris@gmail.com>
2019-09-16 14:41:02 -0700

    Updates to assignment 4

--- assignment4/code/BurrowsWheeler.java
+++ assignment4/code/BurrowsWheeler.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
+
 /**
- * 
+ * Class that implements the Burrows-Wheeler encoding/decoding of strings
  * @author ckingsley
  *
  */
@@ -9,8 +11,34 @@ public class BurrowsWheeler {
      * Apply Burrows-Wheeler encoding, reading from standard input and writing to standard output
      */
 	public static void encode() {
-    	
+		CircularSuffixArray circArray = new CircularSuffixArray("ABRACADABRA!");
+		char[] encoding = new char[ circArray.length() ];
+		int first = -1;
+		
+		for (int i = 0; i < circArray.length(); i++) {
+			if (circArray.index(i) == 0) {
+				first = i;
+			}
+			
+			int pos = (circArray.index(i) + circArray.length() - 1) % circArray.length();
+			encoding[i] = circArray.getText()[pos];
+//			encoding[i] = getLastCharAt(circArray.getText(), circArray.index(i));
+		}
+		
+		System.out.println(first + " " + Arrays.toString(encoding));
     }
+	
+//	/**
+//	 * Get the last character from the permuted string at the specified index in the circular array
+//	 * @param text original unencoded text
+//	 * @param index array index of the circular permuted 
+//	 * @return
+//	 */
+//	private static char getLastCharAt(char[] text, int index) {
+//		int pos = (index + text.length - 1) % text.length;
+//		
+//		return text[pos];
+//	}
 
     /**
      * Apply Burrows-Wheeler decoding, reading from standard input and writing to standard output
@@ -28,14 +56,15 @@ public class BurrowsWheeler {
      * @param args command line args
      */
     public static void main(String[] args) {
+//    	encode(); System.exit(0);
     	if (args == null || args.length == 0) {
     		System.out.println("Usage: BurrowsWheeler +/- stdin");
     		System.exit(1);
     	}
     	
-    	if ("+".equals(args[0])) {
+    	if ("-".equals(args[0])) {
     		encode();
-    	} else if ("-".equals(args[0])) {
+    	} else if ("+".equals(args[0])) {
     		decode();
     	} else {
     		System.out.println("Invalid argument to BurrowsWheeler - must be + or -");
--- assignment4/code/CircularSuffixArray.java
+++ assignment4/code/CircularSuffixArray.java
@@ -1,27 +1,38 @@
+import java.util.Arrays;
+
 /**
  * 
  * @author ckingsley
  *
  */
 public class CircularSuffixArray {
-	private String inputString;
-	
+	private final char[] text;
+	private Suffix[] suffixes;
 	
 	/**
 	 * Constructor for a circular suffix array of s
 	 * @param s the string to generate the circular suffix array
 	 */
 	public CircularSuffixArray(String s) {
-		inputString = new String(s);
+		text = s.toCharArray();
+		suffixes = new Suffix[ text.length ];
+		for (int i = 0; i < suffixes.length; i++) {
+			suffixes[i] = new Suffix(i);
+		}
+		Arrays.sort(suffixes);
 	}
     
+	
+	public char[] getText() {
+		return text;
+	}
 	
 	/**
 	 * Return the length of the string
 	 * @return length of the string
 	 */
 	public int length() {
-		return(inputString.length());
+		return(text.length);
 	}
     
 	
@@ -31,6 +42,68 @@ public class CircularSuffixArray {
 	 * @return index of ith sorted suffix
 	 */
 	public int index(int i) {
-    	return(0);
+    	return(suffixes[i].startIndex);
     }
+	
+	
+	/**
+	 * Inner class representing the circular permuted strings
+	 * @author ckingsley
+	 *
+	 */
+	private class Suffix implements Comparable<Suffix> {
+		int startIndex;
+		
+		/**
+		 * Constructor
+		 * @param startIndex starting index of the circularly permuted string for this suffix
+		 */
+		Suffix (int startIndex) {
+			this.startIndex = startIndex;
+		}
+		
+		/**
+		 * Comparable implementation to sort permuted strings lexographically
+		 */
+		public int compareTo(Suffix other) {
+			for (int i = 0; i < CircularSuffixArray.this.text.length; i++) {
+				char thisChar = getCharAt(i), otherChar = other.getCharAt(i);
+				if (thisChar > otherChar) {
+					return 1;
+				} else if (thisChar < otherChar) {
+					return -1;
+				}
+			}
+			
+			return 0;
+		}
+		
+		/**
+		 * Returns the character at the specified position in the circularly permuted string
+		 * @param pos position (0 based) in the circularly permuted string
+		 * @return the character at the specified position in the circularly permuted string
+		 */
+		private char getCharAt(int pos) {
+			char[] text = CircularSuffixArray.this.text;
+			pos = startIndex + pos;
+			
+			if (pos >= text.length) {
+				pos = pos - text.length;
+			}
+			
+			return text[pos];
+		}
+	}
+	
+	
+	/**
+	 * main method for debugging
+	 * @param args cmd line args
+	 */
+	public static void main(String[] args) {
+		CircularSuffixArray circArray = new CircularSuffixArray("ABRACADABRA!");
+		for (int i = 0; i < circArray.length(); i++) {
+			System.out.printf("%d %d\n", i, circArray.index(i));
+		}
+	}
 }
